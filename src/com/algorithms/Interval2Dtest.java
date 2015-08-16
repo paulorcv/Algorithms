ae302272e26ee0666f22a6b00655d3fc3955ecd6
Paulo Campos <paulorcv@gmail.com>
2015-08-16 07:42:09 -0300

    Interval2Dtest client

---
+++ src/com/algorithms/Interval2Dtest.java
@@ -0,0 +1,19 @@
+package com.algorithms;
+
+/**
+ * Created by paulorcv on 16/08/15.
+ */
+public class Interval2Dtest {
+
+    public static void main( String [] args){
+
+        double xlo = Double.parseDouble(args[0]);
+        double xhi = Double.parseDouble(args[1]);
+        double ylo = Double.parseDouble(args[2]);
+        double yhi = Double.parseDouble(args[3]);
+
+
+    }
+
+
+}
